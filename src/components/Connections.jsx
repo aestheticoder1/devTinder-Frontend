5c36acfd3fbade6072c7dd7fc9754106f714ad83
Nihal Rawat <nihal.riya03@gmail.com>
2025-02-17 17:18:17 +0530

    Feat: Chat System

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -5,6 +5,7 @@ import { BASE_URL } from "../utils/constants";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addConnections } from "../utils/connectionSlice";
+import { Link } from "react-router-dom";
 
 const Connections = () => {
 
@@ -42,14 +43,19 @@ const Connections = () => {
             {connections.map((connection) => {
                 const { firstName, lastName, photoUrl, age, gender, about } = connection
                 return (
-                    <div key={connection._id} className="flex m-4 p-4 bg-base-300 rounded-lg w-1/2 mx-auto">
-                        <div className="mx-4">
-                            <img src={photoUrl} alt="photo" className="w-20 h-20 rounded-full" />
+                    <div key={connection._id} className="flex justify-between items-center m-4 p-4 bg-base-300 rounded-lg w-1/2 mx-auto">
+                        <div className="flex">
+                            <div className="mx-4">
+                                <img src={photoUrl} alt="photo" className="w-20 h-20 rounded-full" />
+                            </div>
+                            <div className="mx-6 text-left">
+                                <h2 className="font-bold text-xl">{firstName + " " + lastName}</h2>
+                                {age && gender && <p>{age + ", " + gender}</p>}
+                                <p>{about}</p>
+                            </div>
                         </div>
-                        <div className="mx-6 text-left">
-                            <h2 className="font-bold text-xl">{firstName + " " + lastName}</h2>
-                            {age && gender && <p>{age + ", " + gender}</p>}
-                            <p>{about}</p>
+                        <div>
+                            <Link to={"/chat/" + connection._id}><button className="btn btn-outline btn-primary mx-2">Message</button></Link>
                         </div>
                     </div>
                 )
